cd50592b6863cd2804801602c4886b772ba0d2d4
Konstantin Honcharov <k.honcharov@gmail.com>
2022-11-05 07:35:59 +0300

    feat: add projects page component and routing

--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProjectsRoutingModule } from './projects-routing.module';
+import { ProjectsPageComponent } from './components/projects-page/projects-page.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [ProjectsPageComponent],
   imports: [CommonModule, ProjectsRoutingModule],
 })
 export class ProjectsModule {}
