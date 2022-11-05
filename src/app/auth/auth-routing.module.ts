cfe78448dff09e165f5a057dabab1c05ec9d0359
Konstantin Honcharov <k.honcharov@gmail.com>
2022-11-05 07:34:54 +0300

    feat: add login page component and auth routing

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginPageComponent } from './components/login-page/login-page.component';
+
+const routes: Routes = [{ path: '', component: LoginPageComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginPageComponent } from './components/login-page/login-page.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginPageComponent],
   imports: [CommonModule, AuthRoutingModule],
 })
 export class AuthModule {}
