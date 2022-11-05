385396e6d0ec5a578fc649710b90021d796d7065
Konstantin Honcharov <k.honcharov@gmail.com>
2022-11-05 07:32:09 +0300

    feat: add basic core components

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { HeaderComponent } from './components/header/header.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  declarations: [HeaderComponent, PageNotFoundComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [HeaderComponent],
 })
 export class CoreModule {}
