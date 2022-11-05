83580eb92a32601b8c3fd07266f8da40d5682956
Konstantin Honcharov <k.honcharov@gmail.com>
2022-11-05 05:42:58 +0300

    feat: connect to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    CoreModule,
+    SharedModule,
     AppRoutingModule,
     StoreModule.forRoot({}, {}),
     EffectsModule.forRoot([]),
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { CoreRoutingModule } from './core-routing.module';
-
 @NgModule({
   declarations: [],
-  imports: [CommonModule, CoreRoutingModule],
+  imports: [CommonModule],
 })
 export class CoreModule {}
