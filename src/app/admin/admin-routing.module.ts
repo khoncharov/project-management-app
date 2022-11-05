b095af590b2ce72c45d7fd8c1bccba814ce77284
Konstantin Honcharov <k.honcharov@gmail.com>
2022-11-05 07:34:15 +0300

    feat: add admin page component and routing

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AdminPageComponent } from './components/admin-page/admin-page.component';
+
+const routes: Routes = [{ path: '', component: AdminPageComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { AdminPageComponent } from './components/admin-page/admin-page.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [AdminPageComponent],
   imports: [CommonModule, AdminRoutingModule],
 })
 export class AdminModule {}
