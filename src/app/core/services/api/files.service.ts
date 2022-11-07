5fd58d65aa3572eb457a7ed1c528c3e189ce0e38
Konstantin Honcharov <k.honcharov@gmail.com>
2022-11-07 19:49:51 +0300

    fix: fix after tests

--- src/app/core/services/api/files.service.ts
+++ src/app/core/services/api/files.service.ts
@@ -1,9 +1,14 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { environment } from '../../../../environments/environment';
-import { httpOptionsWithFormData } from './utils';
+
+export const httpOptionsLoad = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/octet-stream',
+  }),
+};
 
 @Injectable({
   providedIn: 'root',
@@ -11,14 +16,18 @@ import { httpOptionsWithFormData } from './utils';
 export class FilesService {
   constructor(private http: HttpClient) {}
 
-  getFile(taskId: string, filename: string): Observable<unknown> {
+  getFile(taskId: string, filename: string) {
     const url = `${environment.API_ORIGIN}/file/${taskId}/${filename}`;
-    return this.http.get<unknown>(url);
+    return this.http.get(url, httpOptionsLoad);
   }
 
-  uploadFile(taskId: string, file: string): Observable<void> {
+  uploadFile(taskId: string, file: File): Observable<any> {
     const url = `${environment.API_ORIGIN}/file`;
-    const body = { taskId, file };
-    return this.http.post<void>(url, body, httpOptionsWithFormData);
+
+    const formData = new FormData();
+    formData.append('taskId', taskId);
+    formData.append('file', file);
+
+    return this.http.post(url, formData);
   }
 }
