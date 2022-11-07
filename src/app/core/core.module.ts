620541c9281e2bf464c40949eab97b12ef760e6a
Konstantin Honcharov <k.honcharov@gmail.com>
2022-11-07 03:16:55 +0300

    feat: add http client module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { HeaderComponent } from './components/header/header.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [HeaderComponent, PageNotFoundComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, HttpClientModule],
   exports: [HeaderComponent],
 })
 export class CoreModule {}
