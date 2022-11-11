c571f0b5b279b50a8cdde612ab9e097cb3554da0
Konstantin Honcharov <k.honcharov@gmail.com>
2022-11-12 00:23:44 +0300

    feat: add app initial token check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+import * as AuthActions from './store/actions/auth.actions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private store: Store) {}
+
+  ngOnInit(): void {
+    this.store.dispatch(AuthActions.checkToken());
+
+    // TODO: initial redirect
+  }
+}
