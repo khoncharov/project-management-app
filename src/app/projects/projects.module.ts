ab9d65dabf7580cb53338f2d785da146536f03b0
Konstantin Honcharov <k.honcharov@gmail.com>
2022-11-12 19:27:42 +0300

    feat: add initial ui

--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { ProjectsRoutingModule } from './projects-routing.module';
 import { ProjectsPageComponent } from './components/projects-page/projects-page.component';
+import MaterialModule from '../shared/material/material.module';
 
 @NgModule({
   declarations: [ProjectsPageComponent],
-  imports: [CommonModule, ProjectsRoutingModule],
+  imports: [CommonModule, ProjectsRoutingModule, MaterialModule],
 })
 export class ProjectsModule {}
