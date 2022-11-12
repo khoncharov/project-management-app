cf3dcf4a3123ba0ec9e3c5f0e68fd221a8a9c4a3
Konstantin Honcharov <k.honcharov@gmail.com>
2022-11-12 19:28:47 +0300

    feat: add init component

--- src/app/projects/components/projects-page/projects-page.component.ts
+++ src/app/projects/components/projects-page/projects-page.component.ts
@@ -1,8 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+import { Board } from '../../../core/models/board.model';
 
 @Component({
   selector: 'app-projects-page',
   templateUrl: './projects-page.component.html',
   styleUrls: ['./projects-page.component.scss'],
 })
-export class ProjectsPageComponent {}
+export class ProjectsPageComponent implements OnInit {
+  protected boards$!: Observable<Board[]>;
+
+  protected error$!: Observable<string | null>;
+
+  protected isLoading$!: Observable<boolean>;
+
+  ngOnInit(): void {
+    const mock: Board[] = [
+      {
+        id: '1',
+        title: 't1',
+        description: 'd1',
+      },
+      {
+        id: '2',
+        title: 't2',
+        description: 'd2',
+      },
+    ];
+
+    this.boards$ = of(mock);
+    this.error$ = of(null);
+    this.isLoading$ = of(true);
+  }
+}
