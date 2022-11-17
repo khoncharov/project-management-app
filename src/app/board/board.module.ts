7d98c903c295a7434ac20a4eec4751e9d02efc74
Konstantin Honcharov <k.honcharov@gmail.com>
2022-11-17 20:14:09 +0300

    feat: add initial component structure

--- src/app/board/board.module.ts
+++ src/app/board/board.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import MaterialModule from '../shared/material/material.module';
 import { BoardRoutingModule } from './board-routing.module';
 import { BoardPageComponent } from './components/board-page/board-page.component';
 
 @NgModule({
   declarations: [BoardPageComponent],
-  imports: [CommonModule, BoardRoutingModule],
+  imports: [CommonModule, BoardRoutingModule, MaterialModule],
 })
 export class BoardModule {}
