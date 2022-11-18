a1402c90dde0727ea5bafb1b69bbf3d91590c1ea
Konstantin Honcharov <k.honcharov@gmail.com>
2022-11-18 19:44:03 +0300

    feat: create initial board ui

--- src/app/board/board.module.ts
+++ src/app/board/board.module.ts
@@ -4,9 +4,11 @@ import { CommonModule } from '@angular/common';
 import MaterialModule from '../shared/material/material.module';
 import { BoardRoutingModule } from './board-routing.module';
 import { BoardPageComponent } from './components/board-page/board-page.component';
+import { ColumnsInOrderPipe } from './pipes/columns-in-order.pipe';
+import { TasksInOrderPipe } from './pipes/tasks-in-order.pipe';
 
 @NgModule({
-  declarations: [BoardPageComponent],
+  declarations: [BoardPageComponent, ColumnsInOrderPipe, TasksInOrderPipe],
   imports: [CommonModule, BoardRoutingModule, MaterialModule],
 })
 export class BoardModule {}
