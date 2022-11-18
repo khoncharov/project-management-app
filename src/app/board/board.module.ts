575899bac071c21147c94c2d583c4c9487146570
Konstantin Honcharov <k.honcharov@gmail.com>
2022-11-18 21:40:23 +0300

    feat: add board components

--- src/app/board/board.module.ts
+++ src/app/board/board.module.ts
@@ -6,9 +6,17 @@ import { BoardRoutingModule } from './board-routing.module';
 import { BoardPageComponent } from './components/board-page/board-page.component';
 import { ColumnsInOrderPipe } from './pipes/columns-in-order.pipe';
 import { TasksInOrderPipe } from './pipes/tasks-in-order.pipe';
+import { TaskComponent } from './components/task/task.component';
+import { ColumnComponent } from './components/column/column.component';
 
 @NgModule({
-  declarations: [BoardPageComponent, ColumnsInOrderPipe, TasksInOrderPipe],
+  declarations: [
+    BoardPageComponent,
+    ColumnsInOrderPipe,
+    TasksInOrderPipe,
+    TaskComponent,
+    ColumnComponent,
+  ],
   imports: [CommonModule, BoardRoutingModule, MaterialModule],
 })
 export class BoardModule {}
