6ee7ba6f4f93ffc1c62eca76a4f38c235a339add
Konstantin Honcharov <k.honcharov@gmail.com>
2022-11-19 18:28:13 +0300

    feat: add task card styling

--- src/app/board/components/task/task.component.ts
+++ src/app/board/components/task/task.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Store } from '@ngrx/store';
 
-import { TaskShort } from '../../../core/models';
+import { TaskShort, UpdateTaskDto } from '../../../core/models';
 import * as TaskActions from '../../../store/actions/task.actions';
 
 @Component({
@@ -10,12 +10,32 @@ import * as TaskActions from '../../../store/actions/task.actions';
   styleUrls: ['./task.component.scss'],
 })
 export class TaskComponent {
+  @Input() boardId!: string;
+
+  @Input() columnId!: string;
+
   @Input() task!: TaskShort;
 
   constructor(private store: Store) {}
 
-  onTaskEdit(): void {
-    console.log('onTaskEdit');
+  onTaskEdit(boardId: string, columnId: string, taskId: string): void {
+    const task: UpdateTaskDto = {
+      boardId,
+      columnId,
+      order: 1,
+      title: 'new title 1',
+      description: 'desc n-a',
+      userId: 'f894dd8d-3b5d-4769-8eaa-f10eb60cd734',
+    };
+
+    this.store.dispatch(
+      TaskActions.updateTask({
+        boardId,
+        columnId,
+        taskId,
+        task,
+      }),
+    );
   }
 
   onTaskDelete(boardId: string, columnId: string, taskId: string): void {
