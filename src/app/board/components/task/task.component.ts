2cbeb1e9213f7910d57ff6cbf4c4e17441e08e73
Konstantin Honcharov <k.honcharov@gmail.com>
2022-11-19 04:52:20 +0300

    feat: add separate task component

--- src/app/board/components/task/task.component.ts
+++ src/app/board/components/task/task.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+import { TaskShort } from '../../../core/models';
+import * as TaskActions from '../../../store/actions/task.actions';
 
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
   styleUrls: ['./task.component.scss'],
 })
-export class TaskComponent {}
+export class TaskComponent {
+  @Input() task!: TaskShort;
+
+  constructor(private store: Store) {}
+
+  onTaskEdit(): void {
+    console.log('onTaskEdit');
+  }
+
+  onTaskDelete(boardId: string, columnId: string, taskId: string): void {
+    this.store.dispatch(TaskActions.deleteTask({ boardId, columnId, taskId }));
+  }
+}
