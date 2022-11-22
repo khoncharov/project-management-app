1271d3abd5bae2a6bb7b6e9a54bc33d91e52f2e6
Konstantin Honcharov <k.honcharov@gmail.com>
2022-11-22 06:29:40 +0300

    feat: get user date on app init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
 import * as AuthActions from './store/actions/auth.actions';
+import * as UserActions from './store/actions/user.actions';
+import * as fromCurrentUser from './store/selectors/current-user.selectors';
 
 @Component({
   selector: 'app-root',
@@ -9,11 +12,19 @@ import * as AuthActions from './store/actions/auth.actions';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  private userId$!: Observable<string>;
+
   constructor(private store: Store) {}
 
   ngOnInit(): void {
     this.store.dispatch(AuthActions.checkToken());
 
+    this.userId$ = this.store.select(fromCurrentUser.selectUserId);
+    this.userId$.subscribe((id) => {
+      this.store.dispatch(UserActions.getUser({ id }));
+    });
+
     // TODO: initial redirect
+    // TODO: unsub
   }
 }
