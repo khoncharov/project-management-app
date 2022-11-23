26c6fe70f1d1c0f81a9256d9e47057a3c16c46bf
Konstantin Honcharov <k.honcharov@gmail.com>
2022-11-23 04:15:33 +0300

    feat: add sorting pipe for board list

--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -6,9 +6,14 @@ import { ProjectsRoutingModule } from './projects-routing.module';
 import { ProjectsPageComponent } from './components/projects-page/projects-page.component';
 import MaterialModule from '../shared/material/material.module';
 import { BoardDialogComponent } from './components/board-dialog/board-dialog.component';
+import { SortBoardsByTitlePipe } from './pipes/sort-boards-by-title.pipe';
 
 @NgModule({
-  declarations: [ProjectsPageComponent, BoardDialogComponent],
+  declarations: [
+    ProjectsPageComponent,
+    BoardDialogComponent,
+    SortBoardsByTitlePipe,
+  ],
   imports: [
     CommonModule,
     ProjectsRoutingModule,
