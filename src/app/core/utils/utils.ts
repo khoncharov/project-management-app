4d9d063f4ec8699f4c814fa58a1c17fd84412025
Konstantin Honcharov <k.honcharov@gmail.com>
2022-11-26 08:08:11 +0300

    fix: remove console log

--- src/app/core/utils/utils.ts
+++ src/app/core/utils/utils.ts
@@ -11,8 +11,7 @@ export const parseJWT = (token: string): TokenData | null => {
     try {
       result = JSON.parse(atob(str));
     } catch {
-      // eslint-disable-next-line no-console
-      console.error('Parse failed - invalid token');
+      // Parse failed - invalid token
     }
     return result;
   }
